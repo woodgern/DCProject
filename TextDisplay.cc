b38fe4bab4b9cade680c09a5ea1d15be5493584c
Matthew Robertson <matthew.d.robertson.95@gmail.com>
2014-11-22 17:35:07 -0500

    Adds rough draft of TextDisplay

--- TextDisplay.cc
+++ TextDisplay.cc
@@ -1,26 +1,33 @@
 #include "TextDisplay.h"
+#include <iostream>
 
-// Constructors/Destructors
-//  
+using namespace std;
 
-TextDisplay::TextDisplay () {
-initAttributes();
+TextDisplay::TextDisplay(Tile *newMap[][], Player *player){
+	this.map = newMap;
+	this.player = player;
 }
 
-TextDisplay::~TextDisplay () { }
-
-//  
-// Methods
-//  
-
-
-// Accessor methods
-//  
-
-
-// Other methods
-//  
-
-void TextDisplay::initAttributes () {
+void TextDisplay::draw (string action, int floorCount){
+	drawMap();
+	drawStatus(action, floorCount);
 }
 
+void TextDisplay::drawMap(){
+	for(int i = 0; i < 25; i++){
+		for(int j = 0; j < 79; j++){
+			cout << map[i][j].getSymbol();
+		}
+		cout << endl;
+	}
+}
+
+void TextDisplay::drawStatus(string action, int floorCount){
+	cout << left << "Race: " << player.getRace();
+	cout << " Gold: " << player.getGold();
+	cout << setw(72) << "Floor " << floorCount << endl;
+	cout << "HP: " << player.getHP() << endl;
+	cout << "Atk: " << player.getAtk() << endl;
+	cout << "Def: " << player.getDef() << endl;
+	cout << "Action: " << action << endl;
+}
