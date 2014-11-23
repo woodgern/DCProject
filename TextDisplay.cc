2d62071983577ad90d0d6c879e65a0f7b2c1851d
Matthew Robertson <matthew.d.robertson.95@gmail.com>
2014-11-22 19:25:15 -0500

    Fixes Merge issue, reinstates TextDisplay

--- TextDisplay.cc
+++ TextDisplay.cc
@@ -1,26 +1,33 @@
 #include "TextDisplay.h"
+#include <iostream>
 
-// Constructors/Destructors
-//  
+using namespace std;
 
-TextDisplay::TextDisplay () {
-initAttributes();
+TextDisplay::TextDisplay(Tile *newMap[][], Player *player){
+	this.map = newMap;
+	this.player = player;
 }
 
-TextDisplay::~TextDisplay () { }
-
-//  
-// Methods
-//  
-
-
-// Accessor methods
-//  
-
-
-// Other methods
-//  
-
-void TextDisplay::initAttributes () {
+void TextDisplay::draw (string action, int floorCount){
+	drawMap();
+	drawStatus(action, floorCount);
 }
 
+void TextDisplay::drawMap(){
+	for(int i = 0; i < 25; i++){
+		for(int j = 0; j < 79; j++){
+			cout << map[i][j].getSymbol();
+		}
+		cout << endl;
+	}
+}
+
+void TextDisplay::drawStatus(string action, int floorCount){
+	cout << left << "Race: " << player.getRace();
+	cout << " Gold: " << player.getGold();
+	cout << setw(72) << "Floor " << floorCount << endl;
+	cout << "HP: " << player.getHealth() << endl;
+	cout << "Atk: " << player.getAttack() << endl;
+	cout << "Def: " << player.getDefense() << endl;
+	cout << "Action: " << action << endl;
+}
--- TextDisplay.h
+++ TextDisplay.h
@@ -3,156 +3,22 @@
 #define TEXTDISPLAY_H
 
 #include <string>
-
-/**
-  * class TextDisplay
-  * 
-  */
+#include "Player.h"
+#include "Tile.h"
 
 class TextDisplay
 {
-public:
+    Tile *map[25][79];
+    Player *player;
 
-  // Constructors/Destructors
-  //  
+    void drawStatus (string action, int floorCount);
+    void drawMap ();
 
+    public:
+    TextDisplay (Tile *newMap[][], Player *player);
+    virtual ~TextDisplay ();
 
-  /**
-   * Empty Constructor
-   */
-  TextDisplay ();
-
-  /**
-   * Empty Destructor
-   */
-  virtual ~TextDisplay ();
-
-  // Static Public attributes
-  //  
-
-  // Public attributes
-  //  
-
-
-  // Public attribute accessor methods
-  //  
-
-
-  // Public attribute accessor methods
-  //  
-
-
-
-  /**
-   */
-  void drawMap ()
-  {
-  }
-
-
-  /**
-   * @param  action
-   * @param  floorCount
-   */
-  void draw (string action, int floorCount)
-  {
-  }
-
-protected:
-
-  // Static Protected attributes
-  //  
-
-  // Protected attributes
-  //  
-
-public:
-
-
-  // Protected attribute accessor methods
-  //  
-
-protected:
-
-public:
-
-
-  // Protected attribute accessor methods
-  //  
-
-protected:
-
-
-private:
-
-  // Static Private attributes
-  //  
-
-  // Private attributes
-  //  
-
-  Tile map;
-  Player player;
-public:
-
-
-  // Private attribute accessor methods
-  //  
-
-private:
-
-public:
-
-
-  // Private attribute accessor methods
-  //  
-
-
-  /**
-   * Set the value of map
-   * @param new_var the new value of map
-   */
-  void setMap (Tile new_var)   {
-      map = new_var;
-  }
-
-  /**
-   * Get the value of map
-   * @return the value of map
-   */
-  Tile getMap ()   {
-    return map;
-  }
-
-  /**
-   * Set the value of player
-   * @param new_var the new value of player
-   */
-  void setPlayer (Player new_var)   {
-      player = new_var;
-  }
-
-  /**
-   * Get the value of player
-   * @return the value of player
-   */
-  Player getPlayer ()   {
-    return player;
-  }
-private:
-
-
-
-  /**
-   * @param  action
-   * @param  floorCount
-   */
-  void drawStatus (string action, int floorCount)
-  {
-  }
-
-  void initAttributes () ;
-
+    void draw (string action, int floorCount);
 };
 
 #endif // TEXTDISPLAY_H
