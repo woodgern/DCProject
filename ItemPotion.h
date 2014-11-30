9f13b7534b5f034fbf3da463dc0460be08063198
Matthew Robertson <matthew.d.robertson.95@gmail.com>
2014-11-30 17:40:29 -0500

    Cleans Potion header

--- ItemPotion.h
+++ ItemPotion.h
@@ -20,30 +20,6 @@ public:
   char getClassName ();
   std::string getDescription ();
   int getType ();
-
-  /**
-   * Set the value of attribute
-   * @param new_var the new value of attribute
-   */
-  void setAttribute (char new_var)   {
-      attribute = new_var;
-  }
-
-  /**
-   * Get the value of attribute
-   * @return the value of attribute
-   */
-  char getAttribute ()   {
-    return attribute;
-  }
-
-  /**
-   * Set the value of potency
-   * @param new_var the new value of potency
-   */
-  void setPotency (int new_var)   {
-      potency = new_var;
-  }
   int getPotency ();
 private:
 
